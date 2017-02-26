114a59c51fd9b6261e40b333e3b97460e8e30a01
Ryan B <ryan.bliton@gmail.com>
2017-02-26 12:22:59 -0500

    game now accepts guesses

--- src/Game.java
+++ src/Game.java
@@ -1,10 +1,22 @@
 
 public class Game {
 	private String answer;
+	private String hits;
+	private String misses;
 	
 	public Game(String answer) {
 		this.answer = answer;
+		this.hits ="";
+		this.misses ="";
 	}
 	
-	Game game = new Game("answer");
+	public boolean applyGuess(char guess){
+		boolean isHit = answer.indexOf(guess) != -1;
+		if (isHit){
+			hits += guess;
+		} else {
+			misses += guess;
+		}
+		return isHit;
+	}
 }
