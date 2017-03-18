acc060aa53e6720400c7225ba4040137560ef8eb
Ryan B <ryan.bliton@gmail.com>
2017-03-18 17:18:04 -0500

    prompter functionality

--- src/Hangman.java
+++ src/Hangman.java
@@ -3,7 +3,13 @@ public class Hangman {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Game game = new Game("testing");
+		Prompter prompter = new Prompter(game);
+		boolean isHit = prompter.promptForGuess();
+		if (isHit) {
+			System.out.println("Hit!");
+		} else {
+			System.out.println("Miss!");
+		}
 	}
-
 }
